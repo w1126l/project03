14b932940afef79bd5acae0f252d4d23f13aa361
w1126l <1126winniel@gmail.com>
2019-10-10 12:46:24 -0700

    should work

--- sketch.js
+++ sketch.js
@@ -1,13 +1,43 @@
-var img;
+var egg;
+
 function preload() {
-img = loadImage("images/samoyed.png");
+  egg = loadImage("egg.png");
 }
 
 function setup() {
-  createCanvas(1500, 1400);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(220, 5, 50);
-  image(img, 0, 0);
+  background(230, 270, 270);
+
+  strokeWeight(4);
+  stroke(270, 270, 270, 255);
+
+  fill(0, 0, 0, 20);
+  ellipse(70, 70, 200, 200);
+
+  fill(0, 0, 0, 20);
+  ellipse(200, 150, 300, 300);
+
+  fill(0, 0, 0, 20);
+  ellipse(500, 100, 250, 250);
+
+  fill(0, 0, 0, 20);
+  ellipse(200, 400, 300, 300);
+
+  fill(0, 0, 0, 20);
+  ellipse(90, 600, 500, 500);
+
+  fill(0, 0, 0, 20);
+  ellipse(400, 500, 400, 400);
+
+  fill(0, 0, 0, 20);
+  ellipse(500, 250, 200, 200);
+
+  fill(270, 270, 270, 200);
+  strokeWeight(0);
+  rect(70, 80, 350, 250);
+
+  image(egg, 15, 60);
 }
